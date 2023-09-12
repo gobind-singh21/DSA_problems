f0517e9fb749bfd536f66a90d8f7146c6e5d18fd
gobind-singh21 <sgobind577@gmail.com>
2023-09-13 03:11:53 +0530

    new file:   DP/distinctSubsequences.cpp 	new file:   DP/longestCommonSubsequence.cpp 	new file:   DP/longestCommonSubstring.cpp 	new file:   DP/longestPalindromicSubsequence.cpp 	new file:   DP/minInsertionsToMakePalindrome.cpp 	new file:   DP/minOperationForStringAtoB.cpp 	new file:   DP/printLCS.cpp 	new file:   DP/shortestCommonSupersequence.cpp

---
+++ DP/minInsertionsToMakePalindrome.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int minInsertions(string s) {
+        int n = s.size();
+        string s1 = s;
+        reverse(s1.begin(), s1.end());
+        vector<int> prev(n + 1, 0), curr(n + 1, 0);
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n; j++) {
+                if(s[i - 1] == s1[j - 1])
+                    curr[j] = 1 + prev[j - 1];
+                else
+                    curr[j] = max(prev[j], curr[j - 1]);
+            }
+            prev = curr;
+        }
+        int insertions = n - prev[n];
+        return insertions;
+    }
+};
+
+int main() {
+    return 0;
+}
